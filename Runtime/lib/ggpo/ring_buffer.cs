8a56b12b8b516ae12a6876747f3ea0333661a152
james7132 <james.siqi.liu@gmail.com>
2019-10-20 23:23:36 -0700

    Port ring_buffer and static_buffer to C#

---
+++ Runtime/lib/ggpo/ring_buffer.cs
@@ -0,0 +1,44 @@
+using UnityEngine.Assertions;
+
+namespace HouraiTeahouse.Backroll {
+
+public class RingBuffer<T> {
+
+  readonly T[] _data;
+
+  public int Size { get; private set; }
+
+  int _head, _tail;
+
+  public RingBuffer(int size) {
+    _data = new T[size];
+    _head = _tail = 0;
+  }
+
+  public ref T Peek() {
+    Assert.IsTrue(Size != _data.Length);
+    return ref _data[_tail];
+  }
+
+  public ref T this[int idx] => ref _data[(_tail + idx) % _data.Length];
+
+  public void Pop() {
+    Assert.IsTrue(Size != _data.Length);
+    _tail = (_tail + 1) % _data.Length;
+    Size--;
+  }
+
+  public void Push(in T val) {
+    Assert.IsTrue(Size != (_data.Length - 1));
+    _data[_head] = t;
+    _head = (_head + 1) % _data.Length;
+    Size++;
+  }
+
+  public bool IsEmpty() => Size == 0;
+
+}
+
+}
+
+#endif
---
+++ Runtime/lib/ggpo/static_buffer.cs
@@ -0,0 +1,30 @@
+using UnityEngine.Assertions;
+
+namespace HouraiTeahouse.Backroll {
+
+public class StaticBuffer<T> {
+
+  readonly T[] _elements;
+  public int Size { get; private set; }
+  public int Capacity => _elements.Length;
+
+  public StaticBuffer(int size) {
+    _elements = new T[size]
+    Size = 0;
+  }
+
+  public ref this[int idx] {
+    get {
+      Assert.IsTrue(idx >= 0 && idx < Size);
+      return ref _elements[idx];
+    }
+  }
+
+  public void Push(in T val) {
+    Assert.IsTrue(Size != (_elements.Length - 1));
+    _elements[Size++] = val;
+  }
+
+}
+
+}
