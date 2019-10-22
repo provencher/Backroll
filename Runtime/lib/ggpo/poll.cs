31187e8f5f764bdf54e0d6726d94047fe0e229ab
james7132 <james.siqi.liu@gmail.com>
2019-10-22 12:39:47 -0700

    Port Poll to C#

---
+++ Runtime/lib/ggpo/poll.cs
@@ -0,0 +1,40 @@
+
+namespace HouraiTeahouse.Backroll {
+
+public class Poll {
+
+  public delegate bool PollHandler(object obj);
+  struct PollSink {
+    public object      Cookie;
+    public PollHandler Handler;
+  }
+
+  readonly List<PollSink> _loop_sinks;
+
+  public Poll() {
+    _loop_sinks = new List<PollSink>();
+  }
+
+  public void Run() {
+     while (Pump()) continue;
+  }
+
+  public void RegisterLoop(PollHandler handler, object cookie = null) {
+    _loop_sinks.Add(new PollSink {
+        Handler = handler,
+        Cookie = cookie
+    });
+  }
+
+  public bool Pump() {
+    int i, res;
+    bool finished = false;
+    foreach (var sink in PollSink) {
+      finished = !sink.Handler(sink.Cookie) || finished;
+    }
+    return finished;
+  }
+
+}
+
+}
